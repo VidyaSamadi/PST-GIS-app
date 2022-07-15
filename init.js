db2ce6a42666f12f25376f47b954f94f04104780
Vidya Samadi <samadi@clemson.edu>
2022-07-14 23:28:23 -0400

    Update init.js

--- init.js
+++ init.js
@@ -1,6 +1,7 @@
 // All material copyright ESRI, All Rights Reserved, unless otherwise specified.
 // See http://js.arcgis.com/3.15/esri/copyright.txt and http://www.arcgis.com/apps/webappbuilder/copyright.txt for details.
-///////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////
 // Copyright © Esri. All Rights Reserved.
 //
 // Licensed under the Apache License Version 2.0 (the "License");
@@ -51,8 +52,7 @@ var ie = (function() {
     window.location.href = window.location.href.replace('webappviewer', 'webappbuilder');
     return;
   }
-
-  var resources = [];
+var resources = [];
   if (debug) {
     resources.push(weinreUrl);
   }
